8b44c1262384b057e3dafc9837d33d849632833e
pkkj12 <81146709+pkkj12@users.noreply.github.com>
2021-05-28 15:31:19 +0900

    Update MemberDTO.java

--- mini_project/src/main/MemberDTO.java
+++ mini_project/src/main/MemberDTO.java
@@ -1,6 +1,6 @@
 package main;
 
-public class MemberDTO { 
+public class MemberDTO {  // DB에 값을 저장하는 메서드
 	
 	
 	String email;
@@ -10,22 +10,22 @@ public class MemberDTO {
 	
 	
 
-	public String getEmail() {
+	public String getEmail() { // DB에 이메일 받아오기
 		return email;
 	}
-	public void setEmail(String id) {
+	public void setEmail(String id) { // DB에 이메일 저장
 		this.email= id;
 	}
-	public String getPassword() {
+	public String getPassword() {	// DB에 패스워드 받아오기
 		return password;
 	}
-	public void setPassword(String password) {
+	public void setPassword(String password) {	 // DB에 패스워드 저장
 		this.password = password;
 	}
-	public String getNickName() {
+	public String getNickName() {	// DB에 닉네임 받아오기
 		return nickName;
 	}
-	public void setNickName(String nickName) {
+	public void setNickName(String nickName) { // DB에 닉네임 저장
 		this.nickName = nickName;
 	}
 	
